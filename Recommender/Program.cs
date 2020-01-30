ab8f61b3990d6cca5dcef771cad31099a5cd13d0
Robert Jankowski <rj_01@pm.me>
2020-01-29 23:27:58 -0800

    Implement recommender

--- Recommender/Program.cs
+++ Recommender/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Trainers;
 
 namespace Recommender
 {
@@ -6,7 +9,75 @@ namespace Recommender
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mlContext = new MLContext();
+            (var trainingDataView, var testDataView) = LoadData(mlContext);
+            var model = BuildAndTrainModel(mlContext, trainingDataView);
+            EvaluateModel(mlContext, testDataView, model);
+            UseModelForSinglePrediction(mlContext, model);
+            SaveModel(mlContext, trainingDataView.Schema, model);
+        }
+
+        public static (IDataView training, IDataView test) LoadData(MLContext mlContext)
+        {
+            var trainingDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-train.csv");
+            var testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-test.csv");
+
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<MovieRating>(trainingDataPath, hasHeader: true, separatorChar: ',');
+            IDataView testDataView = mlContext.Data.LoadFromTextFile<MovieRating>(testDataPath, hasHeader: true, separatorChar: ',');
+
+            return (trainingDataView, testDataView);
+        }
+
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainingDataView)
+        {
+            var estimator = mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "userIdEncoded", inputColumnName: "userId")
+                .Append(mlContext.Transforms.Conversion.MapValueToKey(outputColumnName: "movieIdEncoded", inputColumnName: "movieId"));
+
+            var options = new MatrixFactorizationTrainer.Options
+            {
+                MatrixColumnIndexColumnName = "userIdEncoded",
+                MatrixRowIndexColumnName = "movieIdEncoded",
+                LabelColumnName = "Label",
+                NumberOfIterations = 20,
+                ApproximationRank = 100
+            };
+
+            var trainerEstimator = estimator.Append(mlContext.Recommendation().Trainers.MatrixFactorization(options));
+
+            Console.WriteLine("=============== Training the model ===============");
+            ITransformer model = trainerEstimator.Fit(trainingDataView);
+
+            return model;
+        }
+
+        public static void EvaluateModel(MLContext mlContext, IDataView testDataView, ITransformer model)
+        {
+            Console.WriteLine("=============== Evaluating the model ===============");
+            var prediction = model.Transform(testDataView);
+            var metrics = mlContext.Regression.Evaluate(prediction, labelColumnName: "Label", scoreColumnName: "Score");
+            Console.WriteLine("Root Mean Squared Error : " + metrics.RootMeanSquaredError.ToString());
+            Console.WriteLine("RSquared: " + metrics.RSquared.ToString());
+        }
+
+        public static void UseModelForSinglePrediction(MLContext mlContext, ITransformer model)
+        {
+            Console.WriteLine("=============== Making a prediction ===============");
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<MovieRating, MovieRatingPrediction>(model);
+
+            // change these values to test
+            var testInput = new MovieRating { userId = 6, movieId = 12 };
+
+            var movieRatingPrediction = predictionEngine.Predict(testInput);
+
+            Console.WriteLine($"Movie {testInput.movieId} is {(Math.Round(movieRatingPrediction.Score, 1) > 3.5 ? "recommended" : "not recommended")} for user {testInput.userId}");
+        }
+
+        public static void SaveModel(MLContext mlContext, DataViewSchema trainingDataViewSchema, ITransformer model)
+        {
+            var modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "MovieRecommenderModel.zip");
+
+            Console.WriteLine("=============== Saving the model to a file ===============");
+            mlContext.Model.Save(model, trainingDataViewSchema, modelPath);
         }
     }
 }
